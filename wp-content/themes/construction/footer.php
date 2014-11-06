5685f19c1340a61840d4dc929852cfd615daa319
Matthew <letemhurryby@users.noreply.github.com>
2014-11-06 01:44:50 -0700

    Working on Nav Home Page
    Navigation suck, may have to wrap in columns, can’t get the containers
    to span single line.  May just leave out footer nag and and keep social

--- wp-content/themes/construction/footer.php
+++ wp-content/themes/construction/footer.php
@@ -11,25 +11,20 @@
 	</div><!-- #content -->
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
-			
-			<div class="row">
-				<nav class="footer-navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
-				</nav>
-			</div>
-
-			<div class="row">
+			<div class"row">
 				
-				<p>Copyright &copy; <?php echo date("Y"); ?> <?php bloginfo ('name'); ?> All Rights Reserved.</p>
-
-			</div>	
-
-			<div class="row">
-				<nav class="social-navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
+				<nav class="footer-navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>	
 				</nav>
+				<div class="coppyright">				
+					<p> Copyright &copy; <?php echo date("Y"); ?> <?php bloginfo ('name'); ?> All Rights Reserved.</p>
+				</div>
+				<nav class="social-navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'social' ) ); ?>
+					test
+				</nav>
+				
 			</div>
-
 		</footer><!-- #colophon -->
 	
 	</div><!-- #page -->
